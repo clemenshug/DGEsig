5a28a5166aa02fcc13202c7c2de3d878bc7f27de
Artem Sokolov <artem.sokolov@gmail.com>
2020-04-17 12:44:26 -0400

    fig1 draft

--- fig1.R
+++ fig1.R
@@ -2,8 +2,7 @@ library( tidyverse )
 
 pathData <- "~/data/DGEsig"
 
-fig1b <- function()
-{
+fig1b <- function() {
     ## Load all results
     R <- file.path(pathData, "clue_results_combined.rds") %>% read_rds() %>%
         filter( result_type == "pert", score_level == "summary" ) %>%
@@ -22,21 +21,51 @@ fig1b <- function()
         select( idQ, idT, tau, source ) %>% group_by( idQ, idT, source ) %>%
         summarize_at( "tau", ~.x[ which.max(abs(.x)) ] ) %>% ungroup() %>%
         mutate_at( c("idQ", "idT"), as.character ) %>%
+        mutate_at( "source", toupper ) %>%
         mutate(drugT = factor(dmap[idT]),
                drugQ = factor(dmap[idQ],
-                              levels=rev(levels(drugT))),
-               self = ifelse(idQ == idT, "self", "other"))
+                              levels=rev(levels(drugT))))
 
-    ## colorRampPalette(rev(brewer.pal(n = 7, name = "RdYlBu")))(100)
-
+    ## Plotting elements
     pal <- rev(RColorBrewer::brewer.pal(n=7, name="RdBu"))
+    etxt <- function(s, ...) {element_text( size = s, face = "bold", ... )}
+    theme_bold <- function() {
+        theme(axis.text.x = etxt(12, angle=90, hjust=1, vjust=0.5),
+              axis.text.y = etxt(12), axis.title = etxt(14),
+              legend.text = etxt(12), legend.title = etxt(14))
+    }
+
+    ## Plotting a heatmap of clue hits
+    fplot <- function(X) {
+        ggplot( X, aes(x=drugT, y=drugQ, fill=tau) ) +
+            theme_minimal() + theme_bold() +
+            geom_tile(color="black") +
+            geom_tile(data=filter(X, idQ==idT), color="black", size=1) +
+            scale_fill_gradientn( colors=pal, guide=FALSE, limits=c(-100,100) ) +
+            xlab( "Clue Target" )
+    }
     
-    ## Test plot
-    X <- R2 %>% filter(source == "dge")
-    ggplot( X, aes(x=drugT, y=drugQ, fill=tau) ) +
-        theme_minimal() + geom_tile(color="black") +
-        geom_tile(data=filter(X, idQ==idT), color="black", size=1) +
-        scale_fill_gradientn( colors=pal, name="Tau" ) +
-        xlab( "Target" ) + ylab( "Query" ) +
-        theme( axis.text.x = element_text(angle=90, hjust=1, vjust=0.5) )
+    ## DGE plot
+    gg1 <- fplot( filter(R2, source == "DGE") ) +
+        scale_x_discrete(position = "top") +
+        theme(axis.title.x = element_blank()) +
+        ylab( "DGE Query" )
+
+    ## L1000 plot
+    gg2 <- fplot( filter(R2, source == "L1000") ) +
+        ylab( "L1000 Query" )
+
+    ## Summary plot
+    S <- R2 %>% filter(idQ == idT) %>%
+        mutate_at("source", factor, levels=c("L1000","DGE")) %>%
+        mutate_at("source", fct_recode, `Self (DGE)`="DGE", `Self (L1000)`="L1000")
+    ggs <- ggplot( S, aes(x=drugT, y=source, fill=tau) ) +
+        theme_minimal() + theme_bold() +
+        geom_tile(color="black") + ylab("") +
+        scale_fill_gradientn( colors=pal, name="Tau", limits=c(-100,100) ) +
+        theme(axis.text.x = element_blank(), axis.title.x = element_blank())
+
+    ## Create the composite plot
+    ggcomp <- egg::ggarrange( gg1, ggs, gg2, heights=c(6.5,1,6.5) )
+    ggsave( "fig1.pdf", ggcomp, width=7, height=12 )
 }
