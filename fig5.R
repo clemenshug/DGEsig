600be5996fb31ef116826cc9fe3a267ee2bfae76
Artem Sokolov <artem.sokolov@gmail.com>
2020-06-04 17:47:58 -0400

    Figure 5

--- fig5.R
+++ fig5.R
@@ -3,56 +3,85 @@ library( seriation )   # For optimal leaf reordering
 
 pathData <- "~/data/DGEsig"
 
+source("dmap.R")    # Map for standardizing drug names
+                    # (not used in the current code version)
+
+## Load gene sets associated with Steve's drugs
+ct <- cols(lspci_id = col_integer(), query_type = col_character(),
+           cutoff   = col_double(),  cell_id    = col_character())
+GS0 <- file.path(pathData, "all_gene_sets.csv.gz") %>% read_csv( col_types=ct )
+GS <- GS0 %>% filter( grepl("rencell", gene_set), !is.na(symbol) ) %>%
+    group_by( DrugID=lspci_id ) %>% summarize( Set = list(symbol) )
+
 ## Isolate the relevant data chunk
 R0 <- file.path(pathData, "clue_results_dge.rds") %>% read_rds() %>%
     pluck( "data", 4 )
 
 ## Perform additional wrangling
-source("dmap.R")
-R <- R0 %>% filter(dataset_query == "sr_repurposing",
+R <- R0 %>% filter(grepl("rencell", gene_set),
                    !is.na(lspci_id_query),
-                   !is.na(lspci_id_target),
+                   !is.na(lspci_id_target),    
                    pert_type == "trt_cp" ) %>%
-    mutate( drugQ = map_chr(str_split(gene_set, "_"), pluck, 3) ) %>%
-    select( drugQ, drugT = pert_iname, tau,
-           idQ = lspci_id_query, idT = pert_id ) %>%
-    mutate_at( "drugQ", recode, !!!dmap )
+    select( idQ = lspci_id_query, idT = pert_id, tau )
 
 ## Compose tau profiles for each drug
-V <- R %>% group_by( Drug=drugQ ) %>%
-    arrange(idT) %>% summarize( Vals = list(set_names(tau, idT)) )
+## Join against gene sets
+V <- R %>% group_by( DrugID=idQ ) %>% arrange(idT) %>%
+    summarize( Vals = list(set_names(tau, idT)) ) %>%
+    inner_join(GS, by="DrugID")
 
 ## Ensure that all tau values are in the same order
-stopifnot( map( V$Vals, names ) %>% map_lgl(identical, .[[1]]) %>% all )
+stopifnot( map(V$Vals, names) %>% map_lgl(identical, .[[1]]) %>% all )
 
 ## Compute pair-wise similarity for all query drugs
-DST <- crossing(rename_all(V, str_c, "1"),
-                rename_all(V, str_c, "2")) %>%
-    mutate( Similarity = map2_dbl(Vals1, Vals2, cor,
-                            method="pearson", use="complete.obs") )
+tausim <- function( v1, v2 ) cor(v1,v2,method="pearson", use="complete.obs")
+jcrdsim <- function(gs1, gs2)
+    length(intersect(gs1, gs2)) / length(union(gs1, gs2))
+SM <- crossing(rename_all(V, str_c, "1"),
+               rename_all(V, str_c, "2")) %>%
+    mutate(TauSim  = map2_dbl(Vals1, Vals2, tausim),
+           JcrdSim = map2_dbl(Set1, Set2, jcrdsim))
 
 ## Perform hierarchical clustering with optimal leaf reordering
-DM <- DST %>% select( Drug1, Drug2, Similarity ) %>%
-    spread( Drug2, Similarity ) %>% as.data.frame %>%
-    column_to_rownames("Drug1") %>% dist
-lvl <- hclust(DM) %>% reorder(DM) %>% dendextend::order.hclust() %>% labels(DM)[.]
-
-## Fix order of rows and column based on clustering results
-DST <- DST %>% mutate(Drug1 = factor(Drug1, lvl),
-                      Drug2 = factor(Drug2, rev(lvl)))
+## Fixes row order based on the clustering results
+## .df - data frame, .sim - similarity column
+simorder <- function( .df, .sim )
+{
+    ## Compose the distance matrix
+    DM <- .df %>% select( DrugID1, DrugID2, {{.sim}} ) %>%
+        spread( DrugID2, {{.sim}} ) %>% as.data.frame() %>%
+        column_to_rownames("DrugID1") %>% dist()
+    
+    ## Perform hierarchical clustering with optimal leaf reordering
+    lvl <- hclust(DM) %>% reorder(DM) %>%
+        dendextend::order.hclust() %>% labels(DM)[.]
+    
+    ## Fix order of rows and column based on clustering results
+    .df %>% transmute(DrugID1 = factor(DrugID1, lvl),
+                      DrugID2 = factor(DrugID2, rev(lvl)),
+                      Similarity = {{.sim}})
+}
 
 ## Plotting elements
 pal <- rev(RColorBrewer::brewer.pal(n=7, name="RdBu"))
 ebl <- element_blank
 etxt <- function(s, ...) {element_text( size = s, face = "bold", ... )}
 
-## Plot the similarity
-ggplot( DST, aes(Drug1, Drug2, fill=Similarity) ) +
-    theme_minimal() +
-    geom_tile(color="gray") +
-    scale_fill_gradientn( colors=pal, limits=c(-1,1) ) +
-    theme(axis.text.x = etxt(10, angle=90, hjust=1, vjust=0.5),
-          axis.text.y = etxt(10), axis.title=ebl(),
-          legend.text = etxt(12), legend.title=etxt(14)) +
-    ggsave( "fig4.pdf", width=8.5, height=7.75 ) +
-    ggsave( "fig4.png", width=8.5, height=7.75 )
+## Plot a similarity matrix
+simplot <- function( .df )
+{
+    ggplot( .df, aes(DrugID1, DrugID2, fill=Similarity) ) +
+        theme_minimal() +
+        geom_tile(color="gray") +
+        scale_fill_gradientn( colors=pal, limits=c(-1,1) ) +
+        theme(axis.text.x = etxt(10, angle=90, hjust=1, vjust=0.5),
+              axis.text.y = etxt(10), axis.title=ebl(),
+              legend.text = etxt(12), legend.title=etxt(14))
+}
+
+ggtau  <- simorder( SM, TauSim )  %>% simplot() %>%
+    + ggsave( "fig5a.pdf", width=8.5, height=7.75 ) %>%
+    + ggsave( "fig5a.png", width=8.5, height=7.75 )
+ggjcrd <- simorder( SM, JcrdSim ) %>% simplot() %>%
+    + ggsave( "fig5b.pdf", width=8.5, height=7.75 ) %>%
+    + ggsave( "fig5b.png", width=8.5, height=7.75 )
--- wrangle.R
+++ wrangle.R
@@ -4,9 +4,7 @@ synapser::synLogin()
 dloc <- "~/data/DGEsig"
 syn <- synExtra::synDownloader(dloc, ifcollision="overwrite.local")
 
-compound_name_map <- synapser::synGet("syn21586544", downloadLocation=dloc,
-                                      ifcollision="overwrite.local", version = 1)
-
+id_mapping        <- syn("syn22035396")
 cmap_gene_meta    <- syn("syn21547102")
 clue_res_dge      <- syn("syn21907139")
 clue_res_l1000    <- syn("syn21907143")
@@ -15,3 +13,4 @@ diff_exp_by_conc  <- syn("syn21559856")
 pertubation_meta  <- syn("syn21547097")
 signature_meta    <- syn("syn21547101")
 gene_sets         <- syn("syn22105667")
+
