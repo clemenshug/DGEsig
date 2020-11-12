c9f663304e834bfc22393def2ef9a0a60c001f47
Clemens Hug <clemens.hug@gmail.com>
2020-11-12 17:10:30 -0500

    Add comparison of bulk/DGE/CMap gene sets

--- compare_reps.R
+++ compare_reps.R
@@ -1,302 +1,100 @@
 library(tidyverse)
-library(DESeq2)
+# library(DESeq2)
 library(synapser)
-library(data.table)
+library(here)
+# library(data.table)
 
 synLogin()
 
 syn <- synExtra::synDownloader("~/data/DGE_comp/")
 
-deseq_input <- syn("syn21558154") %>%
+dge_gene_sets <- syn("syn22391154") %>%
   read_rds()
 
-sr_drug_repurposing_two_reps <- deseq_input %>%
-  filter(dataset == "lincs_cdk4_6_7") %>%
-  pull(meta_deseq) %>%
-  map("drug_id") %>%
-  {lift_dl(c)(.)} %>%
-  table()
 
-two_rep_meta <- deseq_input %>%
-  filter(dataset == "lincs_cdk4_6_7") %>%
+
+bulk_gene_sets <- syn("syn23554368") %>%
+  read_rds() %>%
+  rowwise() %>%
   mutate(
-    meta_deseq = map(
-      meta_deseq,
-      ~.x %>%
-        filter(drug_id %in% sr_drug_repurposing_two_reps)
-    )
+    gene_set_table = deseq %>%
+      filter(padj < 0.2) %>%
+      list()
   ) %>%
-  mutate(
-    counts_deseq = map2(
-      counts_deseq, meta_deseq,
-      ~.x[, rownames(.y)]
-    )
-  )
+  ungroup()
 
-two_rep_deseq <- two_rep_meta %>%
-  transmute(
-    dataset,
-    data = map2(
-      meta_deseq, counts_deseq,
-      function(m, counts) {
-        m <- m %>%
-          rownames_to_column("sample_id")
-        m %>%
-          filter(drug_id != "control") %>%
-          group_nest(condition, control_condition, .key = "treated_meta") %>%
-          inner_join(
-            m %>%
-              filter(drug_id == "control") %>%
-              group_nest(control_condition, .key = "control_meta"),
-            by = c("control_condition")
-          ) %>%
-          transmute(
-            condition,
-            control_condition,
-            deseq_meta = map2(treated_meta, control_meta, bind_rows),
-            deseq_counts = map2(
-              treated_meta, control_meta,
-              ~counts[, union(.x[["sample_id"]], .y[["sample_id"]])]
-            )
-          )
-      }
-    )
-  ) %>%
-  unnest(data)
-
-unloadNamespace("synapser")
-unloadNamespace("PythonEmbedInR")
-deseq_de <- two_rep_deseq %>%
-  mutate(
-    deseq = map2(
-      deseq_meta, deseq_counts,
-      ~DESeqDataSetFromMatrix(
-        .y,
-        .x %>%
-          mutate(drug_norm = fct_relevel(drug_norm,  "control")) %>%
-          column_to_rownames("sample_id"),
-        design = ~drug_norm
-      ) %>%
-        DESeq()
-    )
-  )
-
-deseq_res <- deseq_de %>%
-  mutate(
-    res = map(
-      deseq,
-      function(x) {
-        res_name <- resultsNames(x)[[2]]
-        res <- results(x, name = res_name)
-        mod <- lfcShrink(x, coef = res_name, res = res, type = "apeglm")
-        mod %>%
-          as.data.frame() %>%
-          rownames_to_column("gene_id") %>%
-          full_join(
-            res %>%
-              as.data.frame() %>%
-              rownames_to_column("gene_id") %>%
-              select(gene_id, log2FoldChange_MLE = log2FoldChange),
-            by = "gene_id"
-          )
-      }
-    )
-  )
-
-
-library(genebabel)
-library(clueR)
-
-comp_res_entrez <- deseq_res %>%
-  mutate(
-    res = map(
-      res,
-      function(df) {
-        df %>%
-          dplyr::rename(ensembl_gene_id = gene_id) %>%
-          {suppressWarnings(join_hgnc(., "ensembl_gene_id", "ensembl_gene_id", c("symbol", "entrez_id")))} %>%
-          dplyr::rename(gene_id = entrez_id)
-      }
-    )
-  )
-
-comp_res_gene_sets <- comp_res_entrez %>%
-  filter(
-    map_lgl(res, ~nrow(filter(.x, padj < 0.05)) > 20)
-  ) %>%
-  mutate(
-    gene_set = condition %>%
-      str_replace_all("\\s", "_") %>%
-      str_replace_all("[^\\w]", ""),
-    res = map(
-      res,
-      ~.x %>%
-        drop_na(padj) %>%
-        arrange(padj) %>%
-        filter(padj < 0.05) %>%
-        mutate(direction = if_else(log2FoldChange < 0, "down", "up"))
-    )
-  )
-
-MAX_PER_QUERY = 25
-comp_res_clue <- comp_res_gene_sets %>%
-  split(ceiling(seq_len(nrow(.)) / MAX_PER_QUERY)) %>%
-  map(
-    ~bind_rows(set_names(.x[["res"]], .x[["gene_set"]]), .id = "gene_set") %>%
-      select(gene_set, gene_id, direction)
-  ) %>%
-  map(
-    clue_gmt_from_df,
-    drop_invalid = TRUE
-  )
-
-comp_res_jobs <- comp_res_clue %>%
-  imap(
-    ~clue_query_submit(
-      .x[["up"]], .x[["down"]],
-      name = paste0("dge_job_", .y),
-      use_fast_tool = FALSE
-    )
-  )
-
-# comp_res_jobs <- c(
-#   "5eb305a5d58eba0011f9031c",
-#   "5eb305a6ed2e4f0011539a5c",
-#   "5eb305a6d58eba0011f9031e",
-#   "5eb305a6ed2e4f0011539a5e",
-#   "5eb305a6d58eba0011f90320",
-#   "5eb305a7ed2e4f0011539a60"
-# )
-
-
-walk(
-  comp_res_jobs,
-  clue_query_wait
-)
-
-comp_res_clue_res <- map_chr(
-  comp_res_jobs,
-  clue_query_download
-)
-
-
-clue_res <- crossing(
-  score_type = "tau",
-  result_type = c("pert", "pcl"),
-  score_level = c("cell", "summary")
-) %>%
-  mutate(
-    data = pmap(
-      .,
-      function(...) {
-        comp_res_clue_res %>%
-          map(clue_parse_result, ...) %>%
-          bind_rows()
-      }
-    )
-  )
-
-
-cmap_signatures <- syn("syn21747571") %>%
+cmap_gene_sets <- syn("syn22391159") %>%
   read_rds()
 
-signature_meta <- syn("syn21547101") %>%
+cmap_pertubation_meta <- syn("syn21547097") %>%
   read_csv()
 
-selected_signatures <- signature_meta %>%
-  filter(lspci_id %in% sr_drug_repurposing_two_reps)
+# canonical_table <- syn("syn20835543") %>%
+#   read_rds() %>%
+#   filter(fp_name == "morgan_normal") %>%
+#   chuck("data", 1)
 
-zscores_selected_signatures <- cmap_signatures %>%
-  select(entrez_id, one_of(selected_signatures[["sig_id"]])) %>%
-  gather("sig_id", "zscore", -entrez_id) %>%
-  inner_join(
-    selected_signatures %>%
-      select(sig_id, lspci_id, cell_id),
-    by = "sig_id"
-  )
-
-# Aggregating multiple cells using procedure by 10.1016/j.cell.2017.10.049
-zscores_selected_signatures_agg <- zscores_selected_signatures %>%
-  as.data.table() %>%
-  {
-    # First aggregate replicates within same cell line
-    .[
-      ,
-      .(zscore = mean(zscore)),
-      keyby = c("entrez_id", "lspci_id", "cell_id")
-    ][
-      # Then across cell lines
-      ,
-      .(zscore_agg = quantile(zscore, c(0.67, 0.33), names = FALSE) %>%
-          {.[order(abs(.))[2]]}
-      ),
-      by = c("entrez_id", "lspci_id")
-    ]
-  } %>%
-  as_tibble()
-
-selected_signatures_gene_sets <- tibble(
-  cutoff = c(0.7, 0.8, 0.9, 0.95)
-) %>%
-  mutate(
-    data = map(
-      cutoff,
-      ~zscores_selected_signatures_agg %>%
-        filter(abs(zscore_agg) > qnorm(.x)) %>%
-        arrange(desc(abs(zscore_agg))) %>%
-        transmute(
-          lspci_id,
-          gene_id = as.character(entrez_id),
-          direction = if_else(zscore_agg > 0, "up", "down"),
-          gene_set = paste0(lspci_id, "_", .x),
-          zscore_agg
-        ) %>%
-        {suppressWarnings(join_hgnc(., "gene_id", "entrez_id", "symbol"))}
-    )
+gene_set_comparison <- dge_gene_sets %>%
+  filter(
+    concentration_method == "concentration_aggregated",
+    replicate_method == "per_replicate",
+    is.na(stim),
+    !is.na(lspci_id)
   ) %>%
-  unnest(data) %>%
-  group_nest(gene_set, lspci_id, cutoff)
-
-two_rep_condition_meta <- two_rep_meta %>%
-  pull(meta_deseq) %>%
-  bind_rows() %>%
-  select(condition, lspci_id, cells, stim, drug_conc) %>%
-  distinct() %>%
-  as_tibble()
-
-gene_set_comparison <- selected_signatures_gene_sets %>%
-  filter(cutoff == 0.7) %>%
-  transmute(lspci_id, method = "cmap", data = as.list(data)) %>%
+  # Keep latest time point
+  # group_by(lspci_id, cells, dataset) %>%
+  # arrange(desc(time), .by_group = TRUE) %>%
+  # slice_head(1) %>%
+  # ungroup() %>%
+  transmute(
+    technique = "dge", lspci_id, cells, replicate = as.factor(paste(dataset, time, replicate, sep = "_")),
+    gene_set = gene_set_table %>%
+      map(select, direction, entrezgene_id)
+  ) %>%
   bind_rows(
-    comp_res_gene_sets %>%
-      select(condition, data = res) %>%
-      left_join(
-        two_rep_condition_meta,
-        by = "condition"
+    cmap_gene_sets %>%
+      filter(
+        replicate_method == "per_replicate",
+        cell_aggregate_method == "per_cell_line",
+        cutoff == 0.7
       ) %>%
-      filter(is.na(stim)) %>%
-      mutate(method = "dge")
+      transmute(
+        technique = "l1000", lspci_id, cells = cell_id, replicate,
+        gene_set = as.list(gene_set_table) %>%
+          map(select, direction, entrezgene_id)
+      )
   )
 
-gene_set_comparison_venn <- gene_set_comparison %>%
-  group_by(lspci_id) %>%
+gene_set_comparison_venn_data <- gene_set_comparison %>%
+  mutate(across(cells, str_to_upper)) %>%
+  # mutate(gene_set = map(gene_set, group_nest, direction, .key = "gene_set")) %>%
+  unnest(gene_set) %>%
+  nest(gene_set = c(direction, entrezgene_id)) %>%
+  arrange(lspci_id, cells) %>%
+  group_by(lspci_id, cells) %>%
+  filter(n() > 1) %>%
   summarize(
-    venn = set_names(
-      map(data, "gene_id"),
-      method
+    # venn = cur_data() %>%
+    venn_data = cur_data() %>%
+      list()
+  )
+
+gene_set_comparison_venn <- gene_set_comparison_venn_data %>%
+  rowwise() %>%
+  mutate(
+    venn = with(
+      venn_data,
+      set_names(
+        map(gene_set, "entrezgene_id"),
+        paste(technique, replicate, sep = "_")
+      )
     ) %>%
       map(na.omit) %>%
       map(head, n = 300) %>%
-      VennDiagram::venn.diagram(filename = NULL) %>%
+      possibly(VennDiagram::venn.diagram, NULL)(filename = NULL) %>%
       list()
   ) %>%
-  left_join(
-    signature_meta %>%
-      group_by(lspci_id) %>%
-      dplyr::slice(1) %>%
-      ungroup() %>%
-      select(lspci_id, pert_iname)
-  )
+  ungroup()
 
 pwalk(
   gene_set_comparison_venn,
@@ -306,9 +104,299 @@ pwalk(
 )
 
 
+make_upset <- function(df, ...) {
+  browser()
+  df %>%
+    transmute(
+      name,
+      gene_set = map(
+        gene_set, head, n = 300
+      )
+    ) %>%
+    unnest(gene_set) %>%
+    mutate(dummy = TRUE) %>%
+    distinct(entrezgene_id, name, dummy) %>%
+    pivot_wider(
+      id_cols = entrezgene_id,
+      names_from = name,
+      values_from = dummy,
+      values_fill = FALSE
+    ) %>%
+    select(-entrezgene_id) %>%
+    as.data.frame() %>%
+    # UpSetR::upset(
+    #   nsets = ncol(.),
+    #   keep.order = FALSE,
+    #   order.by = "freq",
+    #   group.by = "degree",
+    #   ...
+    # )
+    ComplexUpset::upset(intersect = colnames(.), ...)
+}
 
-clue_res_l1000 <- syn("syn21907143") %>%
-  read_rds()
+gene_set_comparison_upset_data <- gene_set_comparison_venn_data %>%
+  left_join(
+    cmap_pertubation_meta %>%
+      distinct(lspci_id, pert_iname),
+    by = "lspci_id"
+  ) %>%
+  filter(map_lgl(venn_data, ~length(unique(.x[["technique"]])) >= 2))
 
-clue_comparison <-
+gene_set_comparison_upset <- gene_set_comparison_upset_data %>%
+  # head(n = 3) %>%
+  mutate(
+    venn = map(
+      venn_data,
+      make_upset
+    )
+  )
+
+pwalk(
+  gene_set_comparison_upset,
+  function(pert_iname, venn, cells, ...) {
+    withr::with_pdf(
+      paste0("upset_de_genes_", pert_iname, "_", cells, ".pdf"),
+      width = 8, height = 5,
+      print(venn)
+    )
+    # png(paste0("upset_de_genes_", pert_iname, "_", cells, ".png"))
+    # print(venn)
+    # dev.off()
+    # ggsave(paste0("upset_de_genes_", pert_iname, ".png"), venn)
+  }
+)
+
+
+alvocidib_venn_data <- gene_set_comparison_upset_data %>%
+  filter(
+    pert_iname == "alvocidib",
+    cells == "MCF7"
+  ) %>%
+  chuck("venn_data", 1) %>%
+  filter(
+    !replicate %in% c("lincs_cdk4_6_7_NA_5", "lincs_cdk4_6_7_NA_6")
+  ) %>%
+  group_by(technique) %>%
+  mutate(
+    name = paste0(technique, "_", 1:n()) %>%
+      as.factor()
+  ) %>%
+  ungroup() %>%
+  arrange(desc(name))
+
+alvocidib_venn <- alvocidib_venn_data %>%
+  make_upset(name = "Gene sets", set_sizes = FALSE, keep_empty_groups = FALSE, min_size = 10, sort_sets = FALSE)
+
+ggsave(
+  "upset_de_genes_alvocidib_MCF7_select_conditions.png",
+  alvocidib_venn,
+  width = 8, height = 5
+)
+
+alvocidib_clue_gmt <- alvocidib_venn_data %>%
+  unnest(gene_set) %>%
+  rename(gene_set = name, gene_id = entrezgene_id) %>%
+  clueR::clue_gmt_from_df()
+
+alvocidib_clue_job <- clueR::clue_query_submit(
+  alvocidib_clue_gmt[["up"]], alvocidib_clue_gmt[["down"]], use_fast_tool = FALSE,
+  name = "alvo"
+)
+
+alvocidib_clue_dl <- clueR::clue_query_download(alvocidib_clue_job)
+
+alvocidib_clue_res <- clueR::clue_parse_result(
+  alvocidib_clue_dl, score_level = "summary", result_type = "pert", score_type = "tau"
+) %>%
+  mutate(
+    gene_set = recode(
+      gene_set,
+      !!!set_names(
+        sort(unique(as.character(alvocidib_venn_data[["name"]]))),
+        paste0("X", 1:length(unique(alvocidib_venn_data[["name"]])))
+      )
+    )
+  )
+
+alvocidib_clue_cor <- alvocidib_clue_res %>%
+  filter(pert_type == "trt_cp") %>%
+  select(pert_id, gene_set, tau) %>%
+  spread(gene_set, tau, fill = 0) %>%
+  column_to_rownames("pert_id") %>%
+  as.matrix() %>%
+  cor(method = "spearman")
+
+alvocidib_clue_cor_hm <- alvocidib_clue_cor %>%
+  pheatmap::pheatmap()
+
+
+ensembl <- biomaRt::useEnsembl(biomart = "ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl", version = "98")
+
+all_ensembl_ids <- bulk_gene_sets %>%
+  pull(gene_set_table) %>%
+  map("ensembl_gene_id") %>%
+  reduce(union)
+
+gene_id_mapping <- biomaRt::getBM(
+  c("entrezgene_id", "ensembl_gene_id"),
+  filters = "ensembl_gene_id",
+  values = all_ensembl_ids,
+  mart = ensembl
+)
+
+unloadNamespace("biomaRt")
+unloadNamespace("AnnotationDBI")
+
+gene_id_mapping_valid <- gene_id_mapping %>%
+  drop_na()
+
+
+bulk_drugs_lspci_id_map <- c(
+  palbociclib = 91464,
+  abemaciclib = 94539,
+  ribociclib = 89588
+)
+
+bulk_dge_l1000_upset_data <- bulk_drugs_lspci_id_map %>%
+  enframe("drug", "lspci_id") %>%
+  mutate(
+    venn_data = map2(
+      lspci_id, drug,
+      ~bind_rows(
+        cmap_gene_sets %>%
+          filter(
+            cell_aggregate_method == "per_cell_line",
+            replicate_method == "replicates_aggregated",
+            lspci_id == .x,
+            cell_id == "MCF7",
+            cutoff == 0.7
+          ) %>%
+          transmute(name = paste("cmap", seq_len(n()), sep = "_"), gene_set = as.list(gene_set_table)),
+        dge_gene_sets %>%
+          filter(
+            concentration_method == "concentration_aggregated",
+            replicate_method == "replicates_aggregated",
+            cells == "MCF7",
+            lspci_id == .x,
+            is.na(stim)
+            # time == 24
+          ) %>%
+          transmute(name = paste("dge", seq_len(n()), sep = "_"), gene_set = as.list(gene_set_table)),
+        bulk_gene_sets %>%
+          filter(
+            drug == .y,
+            cells == "MCF7",
+            time == 24
+          ) %>%
+          rowwise() %>%
+          mutate(
+            gene_set_table = gene_set_table %>%
+              inner_join(gene_id_mapping_valid, by = "ensembl_gene_id") %>%
+              arrange(padj) %>%
+              list()
+          ) %>%
+          ungroup() %>%
+          transmute(name = paste("bulk", seq_len(n()), sep = "_"), gene_set = gene_set_table)
+      )
+    )
+  )
+
+bulk_dge_l1000_upset <- bulk_dge_l1000_upset_data %>%
+  # head(n = 3) %>%
+  mutate(
+    venn = map(
+      venn_data,
+      make_upset
+    )
+  )
+
+pwalk(
+  bulk_dge_l1000_upset,
+  function(drug, venn, ...) {
+    dir.create(here("bulk_dge_cmap_comparison"), showWarnings = FALSE)
+    ggsave(
+      here("bulk_dge_cmap_comparison", paste0(drug, "_threshold0.2.pdf")),
+      venn,
+      width = 7, height = 5
+    )
+  }
+)
+
+
+bulk_dge_l1000_upset_data <- bulk_drugs_lspci_id_map %>%
+  enframe("drug", "lspci_id") %>%
+  mutate(
+    venn_data = map2(
+      lspci_id, drug,
+      ~bind_rows(
+        cmap_gene_sets %>%
+          filter(
+            cell_aggregate_method == "per_cell_line",
+            replicate_method == "per_replicate",
+            lspci_id == .x,
+            cell_id == "MCF7",
+            cutoff == 0.7
+          ) %>%
+          transmute(
+            name = paste("cmap", seq_len(n()), sep = "_"),
+            gene_set = map(
+              gene_set_table,
+              arrange, desc(abs(zscore))
+            )
+          ),
+        dge_gene_sets %>%
+          filter(
+            concentration_method == "concentration_aggregated",
+            replicate_method == "per_replicate",
+            cells == "MCF7",
+            lspci_id == .x,
+            is.na(stim)
+            # time == 24
+          ) %>%
+          transmute(
+            name = paste("dge", seq_len(n()), sep = "_"),
+            gene_set = map(
+              gene_set_table,
+              arrange, padj
+            )
+          ),
+        bulk_gene_sets %>%
+          filter(
+            drug == .y,
+            cells == "MCF7",
+            time == 24
+          ) %>%
+          rowwise() %>%
+          mutate(
+            gene_set_table = gene_set_table %>%
+              inner_join(gene_id_mapping_valid, by = "ensembl_gene_id") %>%
+              arrange(padj) %>%
+              list()
+          ) %>%
+          ungroup() %>%
+          transmute(name = paste("bulk", seq_len(n()), sep = "_"), gene_set = gene_set_table)
+      )
+    )
+  )
+
+bulk_dge_l1000_upset <- bulk_dge_l1000_upset_data %>%
+  # head(n = 3) %>%
+  mutate(
+    venn = map(
+      venn_data,
+      make_upset
+    )
+  )
+
+pwalk(
+  bulk_dge_l1000_upset,
+  function(drug, venn, ...) {
+    dir.create(here("bulk_dge_cmap_comparison"), showWarnings = FALSE)
+    ggsave(
+      here("bulk_dge_cmap_comparison", paste0(drug, "_per_replicate", ".pdf")),
+      venn,
+      width = 10, height = 5
+    )
+  }
+)
 
